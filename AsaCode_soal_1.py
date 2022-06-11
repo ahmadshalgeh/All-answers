c91ec03154aafb9bd200a1bb6c86a0f5268bf22f
ahmad shalgeh <39712657+ahmadshalgeh@users.noreply.github.com>
2022-06-12 04:08:11 +0430

    Add files via upload

---
+++ AsaCode_soal_1.py
@@ -0,0 +1,16 @@
+# by python3
+number = input()
+number_list = []
+
+while number_list == sorted(number_list):
+    for i in number:
+        number_list.append(int(i))
+
+    if number_list == sorted(number_list):
+        print(number)
+        break
+
+    else:
+        number = int(number) - 1
+        number = str(number)
+        number_list = []
